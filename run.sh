ddd208a85e2989d09bfe4d24c9d5cee9e584b622
ferrodr <86804978+eskechivoi@users.noreply.github.com>
2022-10-09 18:05:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-sys_kernel=$(uname -s)
-linux="Linux"
-if [ "$sys_kernel" = "$linux" ]; then
-	cd bin
-	java StoreInterface
-else
-	dir bin
-	java StoreInterface
-fi
+cd bin
+java StoreInterface
